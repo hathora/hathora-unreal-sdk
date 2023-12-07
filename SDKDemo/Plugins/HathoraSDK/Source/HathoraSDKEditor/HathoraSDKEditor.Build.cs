ce1a4a77bd9196655fb915f8944c14dbbd3bae9c
Mike Seese <seesemichaelj@gmail.com>
2023-12-07 14:27:02 -0800

    initial implementation of the plugin ui

---
+++ SDKDemo/Plugins/HathoraSDK/Source/HathoraSDKEditor/HathoraSDKEditor.Build.cs
@@ -0,0 +1,54 @@
+// Copyright 2023 Hathora, Inc.
+
+using UnrealBuildTool;
+
+public class HathoraSDKEditor : ModuleRules
+{
+	public HathoraSDKEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"WorkspaceMenuStructure",
+				"InputCore",
+				"HathoraSDK",
+			}
+			);
+
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
